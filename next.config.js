5496a01178e8eeb98d8b0f94c6c14acddd06a680
Kuba <jakub@slys.dev>
2024-01-08 11:07:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'github-pages'
+    output: 'export
+};
 
 module.exports = nextConfig
